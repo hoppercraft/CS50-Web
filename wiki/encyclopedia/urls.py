b16781ab43531789c9f66fc6c5c2133287fc89fd
hoppercraft <09official123@gmail.com>
2025-02-19 20:36:09 +0545

    complete

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-
+import re
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("", views.markdown2html, name="CSS"),
+    path("<str:page>", views.markdown2css, name="markdown2css"),
+    path("search/", views.search, name="search"),
+    path("newpage/", views.newpage, name="newpage"),
+    path("random/", views.random, name="random"),
+    path("pagetoedit/", views.pagetoedit, name="pagetoedit"),
+    path("editpage/", views.editpage, name="editpage")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,17 +1,88 @@
 from django.shortcuts import render
-import markdown
-
 from . import util
-
+import markdown2
+from random import choice
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+    
+def markdown2css(request, page):
+    try:
+        return render(request, "encyclopedia/markdown.html",{"page":markdown2.markdown(util.get_entry(page)),"title":page})
+    except:
+        return render(request, "encyclopedia/error.html", {
+            "message": "Page Not Found"
+        })
+    
+def search(request):
+    if request.method=="POST":
+        entry_search = request.POST['q']
+        try:
+            page = markdown2.markdown(util.get_entry(entry_search))
+            return render(request, "encyclopedia/markdown.html",{
+                "page":page,
+                "title":entry_search
+                })
+        except:
+            check = util.list_entries()
+            recommendation=[]
+            for entry in check:
+                if entry_search.lower() in entry.lower():
+                    recommendation.append(entry)
+            return render(request, "encyclopedia/search.html",{
+                "recommendation":recommendation
+            })
+        
+def newpage(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/newpage.html")
+    else:
+        title = request.POST['title']
+        markdown = request.POST['markdown']
+        titles = util.list_entries()
+        if title in titles:
+            return render(request, "encyclopedia/error.html", {
+                "message": "Entry page already exists"
+            })
+        else:
+            util.save_entry(title, markdown)
+        return render(request, "encyclopedia/markdown.html", {
+            "page":markdown2.markdown(util.get_entry(title)),
+            "title":title
+        })
+    
+def random(request):
+    titles = util.list_entries()
+    title = choice(titles)
+    return render(request, "encyclopedia/markdown.html", {
+        "page":markdown2.markdown(util.get_entry(title)),
+        "title":title
+    })
 
-def markdown2html(CSS):
-    file = util.get_entry(request)
-    mark = markdown.Markdown()
-    if request == None:
-        return None
-    return mark.convert(request)
+def pagetoedit(request):
+    return render(request, "encyclopedia/pagetoedit.html")
+    
+def editpage(request):
+        if request.method == "GET":
+            title=request.GET['title']
+            titles = util.list_entries()
+            if title in titles:
+                return render(request, "encyclopedia/editpage.html", {
+                    "title":title,
+                    "before":util.get_entry(title)
+                })
+            else:
+                 return render(request, "encyclopedia/error.html",{
+                      "message": "Entry page not found"
+                 })
+        else:
+            title = request.POST['title']
+            markdown = request.POST['markdown']
+            util.save_entry(title, markdown)
+        return render(request, "encyclopedia/markdown.html", {
+            "page":markdown2.markdown(util.get_entry(title)),
+            "title":title
+            })
+        
