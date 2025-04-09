a5a30093efeeded707b3a883353faed6f35de71f
hoppercraft <09official123@gmail.com>
2025-04-09 22:29:44 +0545

    lecture sql end

--- lecture/airline/flights/admin.py
+++ lecture/airline/flights/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Flights,Airport,Passenger
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+    list_display=("id","origin", "destination", "duration")
+    
+class PassengerAdmmin(admin.ModelAdmin):
+    filter_horizontal=("flights",)
+
 admin.site.register(Airport)
-admin.site.register(Flights)
-admin.site.register(Passenger)
+admin.site.register(Flights, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmmin)
